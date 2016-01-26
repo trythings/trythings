6b581dd988cfaa4d3a1b07093ce6e1f85057fb5a
Daniel Phan <daniel@trythings.io>
2016-01-26 11:57:54 -0500

    Add script to generate json graphql schema

---
+++ ellies-pad/tools/cmd/updateschema/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/testutil"
+	"github.com/trythings/trythings/ellies-pad/api"
+)
+
+func main() {
+	res := graphql.Do(graphql.Params{
+		Schema:        api.Schema,
+		RequestString: testutil.IntrospectionQuery,
+	})
+	if res.HasErrors() {
+		panic(res.Errors)
+	}
+
+	b, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("./ellies-pad/schema.json", b, 0644) // File mode -rw-r--r--
+	if err != nil {
+		panic(err)
+	}
+}
