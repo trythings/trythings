544d0f1eaf7b4610e5bf3dbd67615676ede429ea
Daniel Phan <daniel@trythings.io>
2016-01-27 09:31:39 -0500

    Add eslint loader

--- ellies-pad/web/webpack.config.js
+++ ellies-pad/web/webpack.config.js
@@ -8,10 +8,15 @@ module.exports = {
 		filename: 'bundle.js',
 	},
 	module: {
+		preLoaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loaders: 'eslint',
+		}],
 		loaders: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
-			loader: 'babel',
+			loaders: 'babel',
 		}],
 	},
 };
