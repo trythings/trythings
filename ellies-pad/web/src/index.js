1dca891963326bc6174e8ede75abdbaa4babfc89
Daniel Phan <daniel@trythings.io>
2016-01-27 11:36:01 -0500

    Add relay route + root container

--- ellies-pad/web/src/index.js
+++ ellies-pad/web/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Relay from 'react-relay';
 
 import App from './App.js';
 
-ReactDOM.render(<App/>, document.getElementById('App'));
+class AppRoute extends Relay.Route {
+	static routeName = 'AppRoute';
+	
+	static queries = {
+		tasks: () => Relay.QL`query { tasks }`,
+	};
+}
+
+const element = (
+	<Relay.RootContainer
+  Component={App}
+  route={new AppRoute()}
+	/>
+);
+
+ReactDOM.render(element, document.getElementById('App'));
