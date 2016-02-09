3de000186e1395440ca75cef62973cf4578d2933
Daniel Phan <daniel@trythings.com>
2016-02-08 19:37:48 -0500

    Add action button to app

--- ellies-pad/web/src/App.js
+++ ellies-pad/web/src/App.js
@@ -4,9 +4,10 @@ import Relay from 'react-relay';
 
 import './Roboto.css';
 
+import ActionButton from './ActionButton.js';
 import Icon from './Icon.js';
 import Task from './Task.js';
-
+import theme from './theme.js';
 
 // TODO: This is a temporary solution to enable us to run all of our migrations.
 class MigrateMutation extends Relay.Mutation {
@@ -101,38 +102,6 @@ class AddTaskMutation extends Relay.Mutation {
 	}
 }
 
-const deepPurple = {
-	500: '#673ab7',
-};
-
-const colors = {
-	primary1: deepPurple['500'],
-	// primary2,
-	// primary3,
-	// accent,
-};
-
-const text = {
-	light: {
-		color: '#ffffff',
-		opacity: {
-			primary: '100%',
-			secondary: '70%',
-			disabled: '50%',
-			dividers: '12%',
-		},
-	},
-	dark: {
-		color: '#000000',
-		opacity: {
-			primary: '87%',
-			secondary: '54%',
-			disabled: '38%',
-			dividers: '12%',
-		},
-	},
-};
-
 class App extends React.Component {
 	static propTypes = {
 		viewer: React.PropTypes.shape({
@@ -146,7 +115,7 @@ class App extends React.Component {
 		title: '',
 		description: '',
 
-		isAddTaskFormVisible: true,
+		isAddTaskFormVisible: false,
 		isMigrateHovering: false,
 	};
 
@@ -203,28 +172,30 @@ class App extends React.Component {
 			flexDirection: 'column',
 		},
 		appBar: {
-			backgroundColor: colors.primary1,
+			backgroundColor: theme.colors.primary1,
 
 			alignItems: 'center',
 			justifyContent: 'space-between',
 
+			boxSizing: 'border-box',
 			display: 'flex',
 			height: 56,
-			minHeight: 'min-content',
-			minWidth: 'min-content',
+			minHeight: 56,
 			paddingLeft: 16,
 			paddingRight: 16,
 
-			// position: 'fixed',
-
 			// App bar hovers above other sheets.
-			boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26)',
-			zIndex: 1,
+			boxShadow: [
+				'0 1px 5px 0 rgba(0, 0, 0, 0.12)', // Ambient.
+				'0 2px 2px 0 rgba(0, 0, 0, 0.14)', // Penumbra.
+				'0 3px 1px -2px rgba(0, 0, 0, 0.20)', // Umbra.
+			].join(','),
+			zIndex: 4,
 
 			title: {
 				// Light primary text.
-				color: text.light.color,
-				opacity: text.light.opacity.primary,
+				color: theme.text.light.color,
+				opacity: theme.text.light.opacity.primary,
 
 				// Title text.
 				fontFamily: 'Roboto, sans-serif',
@@ -238,10 +209,10 @@ class App extends React.Component {
 
 				// backgroundColor: 'rgba(255, 255, 255, 0)',
 				border: 'none',
-				borderRadius: 24,
+				borderRadius: '50%',
 
-				color: text.light.color,
-				opacity: text.light.opacity.primary,
+				color: theme.text.light.color,
+				opacity: theme.text.light.opacity.primary,
 				outline: 0,
 
 				padding: 8,
@@ -249,11 +220,24 @@ class App extends React.Component {
 		},
 		addTaskForm: {
 			backgroundColor: '#ffffff',
-			boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26)',
+			boxShadow: [
+				'0 1px 5px 0 rgba(0, 0, 0, 0.12)', // Ambient.
+				'0 2px 2px 0 rgba(0, 0, 0, 0.14)', // Penumbra.
+				'0 3px 1px -2px rgba(0, 0, 0, 0.20)', // Umbra.
+			].join(','),
 			display: 'flex',
 			minHeight: 'min-content',
 			minWidth: 'min-content',
-			zIndex: 1,
+			zIndex: 6,
+		},
+		addTaskButton: {
+			position: 'absolute',
+			right: 0,
+		},
+		content: {
+			display: 'flex',
+			flexDirection: 'column',
+			overflow: 'scroll',
 		},
 	};
 
@@ -278,39 +262,42 @@ class App extends React.Component {
 					</button>
 				</div>
 
-				{this.state.isAddTaskFormVisible ?
-					(
-						<div style={App.styles.addTaskForm}>
-							<input
-								placeholder="Tags"
-								value={this.state.tags}
-								onChange={this.onTagsChange}
-							/>
-							<input
-								placeholder="Title"
-								value={this.state.title}
-								onChange={this.onTitleChange}
-							/>
-							<textarea
-								placeholder="Description"
-								value={this.state.description}
-								onChange={this.onDescriptionChange}
-							/>
+				<div style={App.styles.content}>
 
-							<div>
-								<button onClick={this.onCancelClick}>
-									Cancel
-								</button>
-								<button onClick={this.onAddClick}>
-									Add Task
-								</button>
+					{this.state.isAddTaskFormVisible ?
+						(
+							<div style={App.styles.addTaskForm}>
+								<input
+									placeholder="Tags"
+									value={this.state.tags}
+									onChange={this.onTagsChange}
+								/>
+								<input
+									placeholder="Title"
+									value={this.state.title}
+									onChange={this.onTitleChange}
+								/>
+								<textarea
+									placeholder="Description"
+									value={this.state.description}
+									onChange={this.onDescriptionChange}
+								/>
+
+								<div>
+									<button onClick={this.onCancelClick}>
+										Cancel
+									</button>
+									<button onClick={this.onAddClick}>
+										Add Task
+									</button>
+								</div>
 							</div>
+						) :
+						<div style={App.styles.addTaskButton}>
+							<ActionButton/>
 						</div>
-					) :
-					null
-				}
+					}
 
-				<div>
 					<ol>
 						{this.props.viewer.tasks.map(task => <Task key={task.id} task={task}/>)}
 					</ol>
