7e5626b9211c6cfba73db84dda2fa220cc28e948
Daniel Phan <daniel@trythings.com>
2016-02-08 19:37:37 -0500

    Move theme constants into theme.js

---
+++ ellies-pad/web/src/theme.js
@@ -0,0 +1,40 @@
+const deepPurple = {
+	500: '#673ab7',
+};
+
+const red = {
+	A400: '#ff1744',
+};
+
+const colors = {
+	primary1: deepPurple['500'],
+	// primary2,
+	// primary3,
+	accent1: red.A400,
+};
+
+const text = {
+	light: {
+		color: '#ffffff',
+		opacity: {
+			primary: '1.00',
+			secondary: '0.70',
+			disabled: '0.50',
+			dividers: '0.12',
+		},
+	},
+	dark: {
+		color: '#000000',
+		opacity: {
+			primary: '0.87',
+			secondary: '0.54',
+			disabled: '0.38',
+			dividers: '0.12',
+		},
+	},
+};
+
+export default {
+	colors,
+	text,
+};
