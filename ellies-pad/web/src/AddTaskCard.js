ef4c0e6573b2c90baea6d900203cab0b4fb34a49
Daniel Phan <daniel@trythings.com>
2016-02-09 19:25:47 -0500

    Remove card styles from AddTaskCard

--- ellies-pad/web/src/AddTaskCard.js
+++ ellies-pad/web/src/AddTaskCard.js
@@ -106,22 +106,6 @@ class AddTaskCard extends React.Component {
 	};
 
 	static styles = {
-		card: {
-			borderRadius: 2,
-			backgroundColor: theme.colors.card,
-			boxShadow: [
-				'0 1px 5px 0 rgba(0, 0, 0, 0.12)', // Ambient.
-				'0 2px 2px 0 rgba(0, 0, 0, 0.14)', // Penumbra.
-				'0 3px 1px -2px rgba(0, 0, 0, 0.20)', // Umbra.
-			].join(','),
-			zIndex: 2,
-
-			boxSizing: 'border-box',
-			display: 'flex',
-			flexDirection: 'column',
-			minHeight: 'min-content',
-			minWidth: 'min-content',
-		},
 		header: {
 			display: 'flex',
 			flexDirection: 'column',
