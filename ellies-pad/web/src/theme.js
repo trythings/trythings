0a116680315853d5cfea615d37565c1d22b3a3f2
Daniel Phan <daniel@trythings.com>
2016-02-10 21:02:23 -0500

    Turn theme text into style objects

--- ellies-pad/web/src/theme.js
+++ ellies-pad/web/src/theme.js
@@ -17,41 +17,48 @@ const grey = {
 const white = '#ffffff';
 
 const colors = {
-	primary: deepPurple['500'],
+	primary: deepPurple[500],
 	accent: red.A400,
 	accentLight: red.A200,
 
-	canvas: grey['300'],
+	canvas: grey[300],
 	card: white,
 };
 
 const text = {
+	fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
 	light: {
-		primary: 'rgba(255, 255, 255, 1.00)',
-		secondary: 'rgba(255, 255, 255, 0.70)',
-		disabled: 'rgba(255, 255, 255, 0.50)',
-		dividers: 'rgba(255, 255, 255, 0.12)',
-
-		color: '#ffffff',
-		opacity: {
-			primary: '1.00',
-			secondary: '0.70',
-			disabled: '0.50',
-			dividers: '0.12',
+		primary: {
+			color: 'rgba(255, 255, 255, 1.00)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		secondary: {
+			color: 'rgba(255, 255, 255, 0.70)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		disabled: {
+			color: 'rgba(255, 255, 255, 0.50)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		dividers: {
+			color: 'rgba(255, 255, 255, 0.12)',
 		},
 	},
 	dark: {
-		primary: 'rgba(0, 0, 0, 0.87)',
-		secondary: 'rgba(0, 0, 0, 0.54)',
-		disabled: 'rgba(0, 0, 0, 0.38)',
-		dividers: 'rgba(0, 0, 0, 0.12)',
-
-		color: '#000000',
-		opacity: {
-			primary: '0.87',
-			secondary: '0.54',
-			disabled: '0.38',
-			dividers: '0.12',
+		primary: {
+			color: 'rgba(0, 0, 0, 0.87)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		secondary: {
+			color: 'rgba(0, 0, 0, 0.54)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		disabled: {
+			color: 'rgba(0, 0, 0, 0.38)',
+			fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+		},
+		dividers: {
+			color: 'rgba(0, 0, 0, 0.12)',
 		},
 	},
 };
@@ -68,6 +75,5 @@ export default {
 			zIndex: 6,
 		},
 	},
-	fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
 	text,
 };
