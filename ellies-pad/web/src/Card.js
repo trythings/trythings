79e792252a8503a68235bf815efa7e7832e0b8e9
Daniel Phan <daniel@trythings.com>
2016-02-10 21:14:40 -0500

    Use reset styles in Card

--- ellies-pad/web/src/Card.js
+++ ellies-pad/web/src/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import resetStyles from './resetStyles.js';
 import theme from './theme.js';
 
 export default class Card extends React.Component {
@@ -9,19 +10,11 @@ export default class Card extends React.Component {
 
 	static styles = {
 		card: {
+			...resetStyles,
+			...theme.elevation[2],
 			borderRadius: 2,
 			backgroundColor: theme.colors.card,
-			boxShadow: [
-				'0 1px 5px 0 rgba(0, 0, 0, 0.12)', // Ambient.
-				'0 2px 2px 0 rgba(0, 0, 0, 0.14)', // Penumbra.
-				'0 3px 1px -2px rgba(0, 0, 0, 0.20)', // Umbra.
-			].join(','),
-			zIndex: 2,
-
-			boxSizing: 'border-box',
-			display: 'flex',
 			flexDirection: 'column',
-			minHeight: 'min-content',
 		},
 	};
 
