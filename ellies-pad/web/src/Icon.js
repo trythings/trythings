6257c4f48e99efadf847c145026e6029ab12d49c
Daniel Phan <daniel@trythings.com>
2016-02-10 21:19:26 -0500

    Reset styles in icon

--- ellies-pad/web/src/Icon.js
+++ ellies-pad/web/src/Icon.js
@@ -2,17 +2,30 @@ import React from 'react';
 
 import './MaterialIcons.css';
 
+import resetStyles from './resetStyles.js';
+
 export default class Icon extends React.Component {
 	static propTypes = {
 		name: React.PropTypes.string.isRequired,
 		color: React.PropTypes.string,
 	};
 
+	static styles = {
+		icon: {
+			...resetStyles,
+			fontFamily: 'default',
+			fontSize: 'default',
+		},
+	};
+
 	render() {
 		return (
 			<i
 				className="material-icons"
-				style={{ color: this.props.color }}
+				style={{
+					...Icon.styles.icon,
+					color: this.props.color,
+				}}
 			>
 				{this.props.name}
 			</i>
