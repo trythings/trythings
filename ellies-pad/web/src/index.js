6fe41242c1480656d1f30fac7b257cfa0aca23c2
Daniel Phan <daniel@trythings.com>
2016-03-23 17:34:55 -0400

    Move normalize import into resetStyles.js

--- ellies-pad/web/src/index.js
+++ ellies-pad/web/src/index.js
@@ -1,5 +1,3 @@
-import 'normalize.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
--- ellies-pad/web/src/resetStyles.js
+++ ellies-pad/web/src/resetStyles.js
@@ -1,3 +1,5 @@
+import 'normalize.css';
+
 const noInherit = {
 	// all: 'initial',
 
