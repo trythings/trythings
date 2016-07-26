03e760b9305d6c3f64ad34ed0d27fc6feda7469a
Daniel Phan <daniel@trythings.com>
2016-07-26 11:25:51 -0400

    Make sign in button redirect afterwards

--- ellies-pad/web/src/SignIn.js
+++ ellies-pad/web/src/SignIn.js
@@ -3,6 +3,12 @@ import gapi from 'gapi';
 import React from 'react';
 
 export default class SignIn extends React.Component {
+	static contextTypes = {
+		router: React.PropTypes.shape({
+			push: React.PropTypes.func.isRequired,
+		}).isRequired,
+	};
+
 	// Routing.
 	static onEnter = (nextState, replace) => {
 		if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
@@ -15,8 +21,25 @@ export default class SignIn extends React.Component {
 		this.id = _uniqueId('SignIn');
 	}
 
+	componentDidMount() {
+		this._isMounted = true;
+		gapi.auth2.getAuthInstance().isSignedIn.listen(this.onSignIn);
+	}
+
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
+
+	onSignIn = (isSignedIn) => {
+		if (this._isMounted && isSignedIn) {
+			this.context.router.push('/');
+		}
+	};
+
 	ref = (div) => {
-		gapi.signin2.render(div.dataset.id);
+		if (div) {
+			gapi.signin2.render(div.dataset.id);
+		}
 	};
 
 	render() {
