16d5ad3323617bc12dcf0644da0af2667efb7115
Daniel Phan <daniel@trythings.com>
2016-08-01 15:56:31 -0400

    Create default spaces, views, and tasks for user

--- ellies-pad/api/apis.go
+++ ellies-pad/api/apis.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/facebookgo/inject"
 	"github.com/facebookgo/startstop"
@@ -18,7 +19,11 @@ type apis struct {
 	TaskAPI   *TaskAPI   `inject:""`
 	UserAPI   *UserAPI   `inject:""`
 
-	UserService *UserService `inject:""`
+	SearchService *SearchService `inject:""`
+	SpaceService  *SpaceService  `inject:""`
+	TaskService   *TaskService   `inject:""`
+	UserService   *UserService   `inject:""`
+	ViewService   *ViewService   `inject:""`
 
 	Schema          *graphql.Schema
 	nodeDefinitions *relay.NodeDefinitions
@@ -95,7 +100,7 @@ func (apis *apis) Start() error {
 						}
 						// TODO Some of the google user's fields could change after user creation.
 						// Consider updating the user to reflect those changes (e.g. IsEmailVerified).
-						err := apis.UserService.Create(p.Context, &User{
+						u := &User{
 							GoogleID:        gu.ID,
 							Email:           gu.Email,
 							IsEmailVerified: gu.EmailVerified,
@@ -103,11 +108,196 @@ func (apis *apis) Start() error {
 							GivenName:       gu.GivenName,
 							FamilyName:      gu.FamilyName,
 							ImageURL:        gu.Picture,
-						})
+						}
+						err := apis.UserService.Create(p.Context, u)
 						if err != nil {
 							return nil, err
 						}
-						// TODO create default space and view
+
+						sp := &Space{
+							Name: fmt.Sprintf("%s's Personal Space", u.GivenName),
+						}
+						err = apis.SpaceService.Create(p.Context, sp)
+						if err != nil {
+							return nil, err
+						}
+
+						v := &View{
+							SpaceID: sp.ID,
+							Name:    "Everything View",
+						}
+						err = apis.ViewService.Create(p.Context, v)
+						if err != nil {
+							return nil, err
+						}
+
+						se := &Search{
+							Name:   "#welcome Search",
+							ViewID: v.ID,
+							Query:  "#welcome",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "Recent Search",
+							ViewID: v.ID,
+							Query:  "CreatedAt >= today() ",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "Everything Else Search",
+							ViewID: v.ID,
+							Query:  "CreatedAt < today() ",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						var t *Task
+
+						t = &Task{
+							Title:   "Recently changed or added tasks will show up in 'Recent'",
+							SpaceID: sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:       "Searches help you find and organize tasks",
+							Description: "#welcome",
+							SpaceID:     sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:       "The same task can show up in multiple searches",
+							Description: "#welcome",
+							SpaceID:     sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:       fmt.Sprintf("Tasks in '%s' are only visible to you", sp.Name),
+							Description: "You can also add people to your space to share it. #welcome",
+							SpaceID:     sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:       "Add #tags to help you find tasks later",
+							Description: "#welcome",
+							SpaceID:     sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						v = &View{
+							SpaceID: sp.ID,
+							Name:    "Priority View",
+						}
+						err = apis.ViewService.Create(p.Context, v)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "Unprioritized Search",
+							ViewID: v.ID,
+							Query:  "NOT (#now OR #next OR #later OR IsArchived: true)",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "#now Search",
+							ViewID: v.ID,
+							Query:  "#now",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "#next Search",
+							ViewID: v.ID,
+							Query:  "#next AND NOT #now",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						se = &Search{
+							Name:   "#later Search",
+							ViewID: v.ID,
+							Query:  "#later AND NOT (#now OR #next)",
+						}
+						err = apis.SearchService.Create(p.Context, se)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:   "Use #now, #next, and #later to prioritize tasks",
+							SpaceID: sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:   "#now means that you're currently working on it",
+							SpaceID: sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:   "#next is the stuff you want to do after you're done with #now",
+							SpaceID: sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						t = &Task{
+							Title:   "#later is for other stuff you want to do eventually",
+							SpaceID: sp.ID,
+						}
+						err = apis.TaskService.Create(p.Context, t)
+						if err != nil {
+							return nil, err
+						}
+
+						return u, nil
 					}
 					return u, err
 				},
