e2dc537463cc765ec9a2b8ca31186ef1ae4be0ee
Annie DuBois <duboisa001@gmail.com>
2016-08-03 07:26:12 -0400

    [perf] Add search results to cache.
    - So that we don't re-run the entire search for numResults.

--- ellies-pad/api/cache.go
+++ ellies-pad/api/cache.go
@@ -8,6 +8,8 @@ import (
 type Cache struct {
 	objs           map[string]interface{}
 	spaceIsVisible map[string]bool
+	// SpaceID -> query -> results
+	searchResults map[string]map[string][]*Task
 }
 
 func (c *Cache) Get(key *datastore.Key) interface{} {
@@ -27,6 +29,7 @@ func (c *Cache) Set(key *datastore.Key, value interface{}) {
 }
 
 // TODO: Looks like each service should maintain its own per-request cache.
+
 // TODO: When we support adding Users to a Space, we'll need to make sure we invalidate these cache entries.
 func (c *Cache) IsVisible(sp *Space) (bool, bool) {
 	if c == nil {
@@ -45,10 +48,40 @@ func (c *Cache) SetIsVisible(sp *Space, isVisible bool) {
 	c.spaceIsVisible[sp.ID] = isVisible
 }
 
+// TODO: Figure out which updates invalidate a search's results (assuming mutations may happen after fetches).
+func (c *Cache) SearchResults(sp *Space, query string) ([]*Task, bool) {
+	if c == nil {
+		return nil, false
+	}
+
+	perQuery := c.searchResults[sp.ID]
+	if perQuery == nil {
+		return nil, false
+	}
+
+	ts, ok := perQuery[query]
+	return ts, ok
+}
+
+func (c *Cache) SetSearchResults(sp *Space, query string, results []*Task) {
+	if c == nil {
+		return
+	}
+
+	perQuery := c.searchResults[sp.ID]
+	if perQuery == nil {
+		perQuery = map[string][]*Task{}
+	}
+	perQuery[query] = results
+
+	c.searchResults[sp.ID] = perQuery
+}
+
 func NewCacheContext(ctx context.Context) context.Context {
 	return context.WithValue(ctx, cacheKey, &Cache{
 		objs:           map[string]interface{}{},
 		spaceIsVisible: map[string]bool{},
+		searchResults:  map[string](map[string][]*Task){},
 	})
 }
 
